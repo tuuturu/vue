b8bff9873236239ade8cf7b5ee1a3b97acbb707f
Ingrid Herigstad <ingrid.herigstad@byr.oslo.kommune.no>
2020-02-16 12:27:19 +0100

    ✅ ADD: Testbutton

--- src/stories/buttons/index.js
+++ src/stories/buttons/index.js
@@ -1,7 +1,10 @@
 import { default as BaseButtonModule } from '~/stories/buttons/BaseButton'
+import { default as TestButtonModule } from '~/stories/buttons/TestButton'
 
 export const BaseButton = BaseButtonModule
+export const TestButton = TestButtonModule
 
 export default {
-	BaseButton
+	BaseButton,
+	TestButton
 }
