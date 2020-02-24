3e949d0a3d8ad4db0c769ebe6e5797f887098714
Ingrid Herigstad <ingrid.herigstad@byr.oslo.kommune.no>
2020-02-24 15:19:42 +0100

    👌 IMPROVE: remove unused components

--- src/stories/forms/index.js
+++ src/stories/forms/index.js
@@ -1,16 +1,10 @@
 import TextInputComponent from './TextInput'
-import DateInputComponent from './DateInput'
-import NumberInputComponent from './NumberInput'
 import TextareaInputComponent from './TextareaInput'
 
-export const DateInput = DateInputComponent
-export const NumberInput = NumberInputComponent
 export const TextInput = TextInputComponent
 export const TextareaInput = TextareaInputComponent
 
 export default {
-	DateInput,
-	NumberInput,
 	TextInput,
 	TextareaInput
 }
