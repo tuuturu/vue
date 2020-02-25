151626858842ee6a915c7780aa5db02c1b01c4b0
Ingrid Herigstad <ingrid.herigstad@byr.oslo.kommune.no>
2020-02-25 12:20:07 +0100

    👌 IMPROVE: remove unused component from stories

--- src/stories/forms/index.js
+++ src/stories/forms/index.js
@@ -1,10 +1,7 @@
 import TextInputComponent from './TextInput'
-import TextareaInputComponent from './TextareaInput'
 
 export const TextInput = TextInputComponent
-export const TextareaInput = TextareaInputComponent
 
 export default {
-	TextInput,
-	TextareaInput
+	TextInput
 }
--- src/stories/links/index.js
+++
@@ -1,6 +0,0 @@
-import { default as LinkComponent } from '~/stories/links/Link'
-
-export const Link = LinkComponent
-export default {
-  Link
-}
