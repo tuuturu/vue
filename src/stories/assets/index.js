5decc1ac5b81f0b2910beff3a8bdf4017f9fa45a
Julius Pedersen <deifyed@gmail.com>
2020-02-26 15:07:03 +0100

    🐛 FIX: asset exports

--- src/stories/assets/index.js
+++ src/stories/assets/index.js
@@ -1,10 +1,13 @@
-import { default as OsloShapeModule } from '~/stories/assets/OsloShape'
-import { default as OsloLogoModule } from '~/stories/assets/OsloLogo'
+import OsloShapeModule from '~/stories/assets/OsloShape'
+import OsloLogoModule from '~/stories/assets/OsloLogo'
+import IconsModule from '~/stories/assets/Icons'
 
 export const OsloShape = OsloShapeModule
 export const OsloLogo = OsloLogoModule
+export const Icons = IconsModule
 
 export default {
   OsloShape,
-  OsloLogo
+  OsloLogo,
+  Icons
 }
