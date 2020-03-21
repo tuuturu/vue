d9ecdbc3fb0bb0d8b25c3ece4ca198cdd532ed0f
Julius Pedersen <deifyed@gmail.com>
2020-03-21 18:26:02 +0100

    🐛 Fix IconLocation not being exported

--- src/stories/icons/index.js
+++ src/stories/icons/index.js
@@ -1,13 +1,16 @@
 import CameraModule from '~/stories/icons/IconCamera'
 import ImageModule from '~/stories/icons/IconImage'
+import LocationModule from '~/stories/icons/IconLocation'
 import LogoModule from '~/stories/icons/IconLogo'
 
 export const IconCamera = CameraModule
 export const IconImage = ImageModule
+export const IconLocation = LocationModule
 export const IconLogo = LogoModule
 
 export default {
 	IconCamera,
   IconImage,
+	IconLocation,
   IconLogo
 }
